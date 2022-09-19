3f79ef9fc57084dfc738adf8e328f1af5f0fa866
Ivan <gubarenkovanek@gmail.com>
2022-09-19 20:32:46 +0300

    dsxzasdas
    asdadasd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const color = getRandomHexColor();
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
+let timerId = null;
+const body = document.querySelector('body');
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.background = randomColor;
+    
+    }, 5000);
+    
+});
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
