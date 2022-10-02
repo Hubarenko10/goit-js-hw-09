60b1efe5b16fd66b46ec38af448741aee0b44eae
Ivan <gubarenkovanek@gmail.com>
2022-10-02 19:55:31 +0300

    reworked color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const color = getRandomHexColor();
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector(['button[data-start]']);
+const stopBtn = document.querySelector(['button[data-stop]']);
 stopBtn.disabled = true;
 let timerId = null;
-const body = document.querySelector('body');
-startBtn.addEventListener("click", () => {
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
-    timerId = setInterval(() => {
-        const randomColor = getRandomHexColor();
-        body.style.background = randomColor;
-    
-    }, 1000);
-    
-});
-stopBtn.addEventListener("click", () => {
+
+startBtn.addEventListener('click', () => { 
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  
+  timerId = setInterval(() => {
+    const changedColor = getRandomHexColor();
+    document.body.style.backgroundColor = changedColor;
+  },1000)
+ });
+
+
+
+stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-});
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+ })
+
+
+
+
+
+
+
